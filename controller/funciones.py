270e814455cb51f8fe54911ed15a17176d07f62e
DW31 <ik012982i9@H012982.NET>
2017-05-17 12:38:24 +0200

    cambios

--- controller/funciones.py
+++ controller/funciones.py
@@ -14,27 +14,35 @@ class Concierto():
 
     def print_artistas(self):
         for x in self.artistas:
-            print(x)
+            return x
 
     def add_sitio(self, sitio):
         self.sitios.append(sitio)
 
     def print_sitios(self):
         for x in self.sitios:
-            print(x)
+            return x
     
     def add_precio(self, precio):
         self.precios.append(precio)
 
-    def print_precios(self):
-        for x in self.precios:
-            print(x)
+    precio = {
+                "zona bronce": [35],
+                "zona plata": [50],
+                "zona oro": [70]
+    }
 
-entradas = {
-            "zona bronce": [35],
-            "zona plata": [50],
-            "zona oro": [70]
-}
+    entradas1 = [
+        {"id": 1, "zona":"zona bronce", "precio": 15 },
+        {"id": 2, "zona":"zona plata", "precio": 30 },
+        {"id": 3, "zona":"zona oro", "precio": 50 }
+    ]
+
+    def print_precios(self):
+        for x in self.precio:
+            for y in self.precio:
+                return self.precio
+
 
 class Entradas():
 
@@ -42,9 +50,6 @@ class Entradas():
         self.entradas = self
 
   
-
-    
-
 ''' main program '''
 
 #s = Artista("Shakira")
@@ -58,7 +63,6 @@ concierto.add_artista("Jason Derulo")
 concierto.add_precio("30 euros")
 concierto.add_precio("50 euros")
 
-#print(concierto.sitios)
 """ seleccion del artista """
 concierto.print_artistas()
 artista = input("Seleccione artista: ")
@@ -70,6 +74,12 @@ lugar = input("Seleccione lugar del concierto: ")
 print("Ha seleccionado el lugar del concierto: " + lugar)
 
 """ seleccion de entrada """
-concierto.print_precios()
 entradas = input("Seleccione el tipo de entrada que desea: ")
 print("Ha seleccionado: " + entradas)
+
+"""dar el precio"""
+print(concierto.print_precios())
+
+#print(concierto.sitios)
+
+#entradas1[0]["id"]
