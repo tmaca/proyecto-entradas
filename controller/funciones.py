0b9fa3e7ca6347ac7e49f61ae626388b3b59085a
DW31 <ik012982i9@H012982.NET>
2017-05-19 09:41:59 +0200

    camb

--- controller/funciones.py
+++ controller/funciones.py
@@ -1,84 +1,69 @@
 class Artista():
-    def __init__(self, name):
+    def init(self, name):
         self.name = name
 
 class Concierto():
 
     def __init__(self):
-        self.artistas = []
-        self.sitios = []
-        self.precios = []
+        self.artistas = ['Shakira', 'Jason Derulo', 'Rihanna', 'Beyoncé']
+        self.sitios = ['Bilbo', 'Barcelona', 'Madrid', 'Valencia', 'Málaga']
+        self.precio = {
+                "zona bronce": 35,
+                "zona plata": 50,
+                "zona oro": 70
+        }
+        self.entradas = []
 
     def add_artista(self, artista):
         self.artistas.append(artista)
 
     def print_artistas(self):
         for x in self.artistas:
-            return x
+            print(x)
 
     def add_sitio(self, sitio):
         self.sitios.append(sitio)
 
     def print_sitios(self):
         for x in self.sitios:
-            return x
-    
-    def add_precio(self, precio):
-        self.precios.append(precio)
+            print(x)
 
-    precio = {
-                "zona bronce": [35],
-                "zona plata": [50],
-                "zona oro": [70]
-    }
-
-    entradas1 = [
-        {"id": 1, "zona":"zona bronce", "precio": 15 },
-        {"id": 2, "zona":"zona plata", "precio": 30 },
-        {"id": 3, "zona":"zona oro", "precio": 50 }
-    ]
-
-    def print_precios(self):
+    def print_zonas(self):
         for x in self.precio:
-            for y in self.precio:
-                return self.precio
+            print(x)
 
+    def print_precios(self, tipo):
+        print(self.precio[tipo])
 
 class Entradas():
 
-    def __init__(self):
-        self.entradas = self
+    def __init__(self, concierto, tipo):
+        self.concierto = concierto
+        self.tipo = tipo
 
-  
 ''' main program '''
 
 #s = Artista("Shakira")
 
 concierto = Concierto()
-#concierto.add_artista(s)
-concierto.add_sitio("Donostia")
-concierto.add_sitio("Bilbo")
-concierto.add_artista("Shakira")
-concierto.add_artista("Jason Derulo")
-concierto.add_precio("30 euros")
-concierto.add_precio("50 euros")
 
 """ seleccion del artista """
 concierto.print_artistas()
-artista = input("Seleccione artista: ")
-print("Ha seleccionado " + artista)
+artista = input("Seleccione artista: "'\n')
+print("Ha seleccionado " + artista + '\n')
 
 """ seleccion del lugar """
 concierto.print_sitios()
-lugar = input("Seleccione lugar del concierto: ")
-print("Ha seleccionado el lugar del concierto: " + lugar)
+lugar = input("Seleccione lugar del concierto: " '\n')
+print("Ha seleccionado el lugar del concierto: " + lugar + '\n')
 
 """ seleccion de entrada """
-entradas = input("Seleccione el tipo de entrada que desea: ")
-print("Ha seleccionado: " + entradas)
+concierto.print_zonas()
+tipo = input("Seleccione el tipo de entrada que desea: " '\n')
+print("Ha seleccionado: " + tipo + '\n')
 
 """dar el precio"""
-print(concierto.print_precios())
+concierto.print_precios(tipo)
 
 #print(concierto.sitios)
 
